9c50c8639cc764910ec1deef7c9be3046939a773
jorgitoluis <jorgitoluisolea@gmail.com>
2023-07-15 06:15:02 -0300

    feat: agrega configuraciones recursos estaticos

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 3030 ;
 const path = require('path');
 
+/*configuraciones */
+app.use(express.static('public'))
+
 /*rutas*/
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
 app.get('/about' , (req,res) => res.sendFile(path.join(__dirname, 'views', 'about')));
